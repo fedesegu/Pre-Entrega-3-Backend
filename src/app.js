ce31f410911ef1f2a4ed2b402441fc6da9d42337
Federico Segú <federicosegu@gmail.com>
2023-12-20 23:17:56 -0300

    modificando

--- src/app.js
+++ src/app.js
@@ -1,67 +1,67 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import { __dirname } from "./utils.js";
-import handlebars from "express-handlebars";
-import viewsRouter from "./routes/views.router.js";
-import cookieRouter from "./routes/cookie.router.js";
-import sessionsRouter from "./routes/sessions.router.js";
-import usersRouter from "./routes/users.router.js"
-import session from "express-session";
-import productsRouter from "./routes/products.router.js";
-import cartsRouter from "./routes/cart.router.js";
-import MongoStore from 'connect-mongo'
+// import express from "express";
+// import cookieParser from "cookie-parser";
+// import { __dirname } from "./utils.js";
+// import handlebars from "express-handlebars";
+// import viewsRouter from "./routes/views.router.js";
+// import cookieRouter from "./routes/cookie.router.js";
+// import sessionsRouter from "./routes/sessions.router.js";
+// import usersRouter from "./routes/users.router.js"
+// import session from "express-session";
+// import productsRouter from "./routes/products.router.js";
+// import cartsRouter from "./routes/cart.router.js";
+// import MongoStore from 'connect-mongo'
 
-import passport from "passport";
-import config from "./config.js"
-const URI = config.mongo_uri
-const PORT = config.port
-
-
-import "./config/configDB.js";
-import "./passport.js";
+// import passport from "passport";
+// import config from "./config.js"
+// const URI = config.mongo_uri
+// const PORT = config.port
 
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
-
-app.use(cookieParser("SecretCookie"));
+// import "./config/configDB.js";
+// import "./passport.js";
 
 
+// const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(__dirname + "/public"));
 
-
-app.use(
-  session({
-   store: new MongoStore({
-      mongoUrl: config.mongo_uri,
-    }),
-    secret: "secretSession",
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60000 },
-  })
-  );
+// app.use(cookieParser("SecretCookie"));
 
 
 
-app.use(passport.initialize());
-app.use(passport.session());
+
+// app.use(
+//   session({
+//    store: new MongoStore({
+//       mongoUrl: config.mongo_uri,
+//     }),
+//     secret: "secretSession",
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { maxAge: 60000 },
+//   })
+//   );
 
 
 
-app.engine("handlebars", handlebars.engine());
-app.set("views", __dirname + "/views");
-app.set("view engine", "handlebars");
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 
-app.use("/", viewsRouter);
 
-app.use("/api/sessions", sessionsRouter);
-app.use("/api/products", productsRouter);
-app.use("/api/cart", cartsRouter);
-app.use("/api/users", usersRouter)
+// app.engine("handlebars", handlebars.engine());
+// app.set("views", __dirname + "/views");
+// app.set("view engine", "handlebars");
 
-app.listen(PORT, () => {
-  console.log("Escuchando al puerto 8080");
-});
+
+// app.use("/", viewsRouter);
+
+// app.use("/api/sessions", sessionsRouter);
+// app.use("/api/products", productsRouter);
+// app.use("/api/cart", cartsRouter);
+// app.use("/api/users", usersRouter)
+
+// app.listen(PORT, () => {
+//   console.log("Escuchando al puerto 8080");
+// });
--- src/config/configDB.js
+++ src/config/configDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import config from "../config.js"
 const URI = config.mongo_uri
-/*const URI = "mongodb+srv://julianbarrera26:G1fxY25230LPwfa0@cluster0.wp6vatq.mongodb.net/ecommerce?retryWrites=true&w=majority";*/
+
 
 mongoose
 .connect(URI)
